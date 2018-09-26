27d96e98164936e2a4fcb946721e7c9c770ff425
yetong <yetongzzz@163.com>
2018-09-26 23:27:05 +0800

    添加了一些文件，方便制造冲突

---
+++ src/main/java/com/zhangyt/Abcd.java
@@ -0,0 +1,14 @@
+package com.zhangyt;
+
+public class Abcd {
+
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
